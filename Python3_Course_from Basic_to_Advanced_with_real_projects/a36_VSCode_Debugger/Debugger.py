6c665651987ff849e7e5b882b228cb74a495768b
wmarenga <wmarenga@yahoo.ca>
2023-09-13 18:34:24 +0200

    a

---
+++ Python3_Course_from Basic_to_Advanced_with_real_projects/a36_VSCode_Debugger/Debugger.py
@@ -0,0 +1,24 @@
+# if / elif      / else
+# If necessary, enable glyph margin (red marker to limit the Debugger).
+
+condition1 = True
+condition2 = True
+condition3 = False
+condition4 = False
+
+if condition1:
+    print('Code for condition 1')
+    print('Code for condition 1')
+elif condition1:
+    print('Cód2go para condição 2')
+elif condition3:
+    print('Code for condition 3')
+elif condition4:
+    print('Code for condition 4')
+else:
+    print('No conditions were satisfied.')
+
+if 10 == 10:
+    print('Other if')
+
+print('Out of if')
