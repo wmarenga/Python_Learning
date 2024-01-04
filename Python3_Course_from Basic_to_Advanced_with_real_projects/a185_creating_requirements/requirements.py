8ea4a550ae69b6cfc5b7f2eed5e02215644f597b
wmarenga <wmarenga44@gmail.com>
2024-01-04 15:09:08 +0100

    english

---
+++ Python3_Course_from Basic_to_Advanced_with_real_projects/a185_creating_requirements/requirements.py
@@ -0,0 +1,14 @@
+"""
+pip uninstall package_name
+
+See package:
+pip freeze
+
+Creating and using the requirements.txt:
+
+pip freeze > requirements.txt
+
+Installing everything from requirements.txt:
+
+pip install -r requirements.txt
+"""
